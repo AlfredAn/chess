c6e2b6e01088c6c1e978a1c972685437a1a040f4
alfredan <alfredan@kth.se>
2016-09-12 19:48:53 +0200

    Made IntVector2 class final

--- ChessLib/src/main/java/indaplusplus/alfredan/hw2and3/chesslib/util/IntVector2.java
+++ ChessLib/src/main/java/indaplusplus/alfredan/hw2and3/chesslib/util/IntVector2.java
@@ -4,7 +4,7 @@ package indaplusplus.alfredan.hw2and3.chesslib.util;
  * An immutable class that contains two integers.
  * Used to represent positions and moves on the chess board.
  */
-public class IntVector2 {
+public final class IntVector2 {
   
   public final int x, y;
   

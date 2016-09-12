73879ae1583f4e1c813aed1d009b1c4d4ad5439a
alfredan <alfredan@kth.se>
2016-09-12 14:24:04 +0200

    Add IntVector2 class

---
+++ ChessLib/src/main/java/indaplusplus/alfredan/hw2and3/chesslib/IntVector2.java
@@ -0,0 +1,15 @@
+package indaplusplus.alfredan.hw2and3.chesslib;
+
+/**
+ * An immutable class that contains two integers.
+ * Used to represent positions and moves on the chess board.
+ */
+public class IntVector2 {
+  
+  public final int x, y;
+  
+  public IntVector2(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+}

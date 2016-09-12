758b825cf9c711229c89e51ec0bc9a553e408eaf
alfredan <alfredan@kth.se>
2016-09-12 20:40:19 +0200

    Renamed ChessBoard unit test class

--- ChessLib/src/test/java/indaplusplus/alfredan/hw2and3/chesslib/ChessBoardJUnitTest.java
+++ ChessLib/src/test/java/indaplusplus/alfredan/hw2and3/chesslib/ChessBoardJUnitTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class ChessBoardJUnit4Test {
+public class ChessBoardJUnitTest {
   
   /**
    * Check that the default empty board is the correct size

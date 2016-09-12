78854c8fe95c973a42a616689c8ad707bf849b04
alfredan <alfredan@kth.se>
2016-09-12 23:01:01 +0200

    A ChessPiece now keeps track of what team it belongs to

--- ChessLib/src/main/java/indaplusplus/alfredan/hw2and3/chesslib/ChessPiece.java
+++ ChessLib/src/main/java/indaplusplus/alfredan/hw2and3/chesslib/ChessPiece.java
@@ -30,7 +30,15 @@ public abstract class ChessPiece {
    */
   int yPos;
   
-  public ChessPiece() {}
+  /**
+   * The team that this piece belongs to.
+   * Normally this can only be either Team.BLACK or Team.WHITE, but any number is valid.
+   */
+  private int team;
+  
+  public ChessPiece(int team) {
+    this.team = team;
+  }
   
   public final int getX() {
     return xPos;
@@ -38,6 +46,14 @@ public abstract class ChessPiece {
   
   public final int getY() {
     return yPos;
+  }
+  
+  public final void setTeam(int team) {
+    this.team = team;
+  }
+  
+  public final int getTeam() {
+    return team;
   }
   
   /**

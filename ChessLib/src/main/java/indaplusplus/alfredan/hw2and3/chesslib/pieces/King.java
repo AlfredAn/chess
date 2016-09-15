af7686958df297274872352ba83f623d74411d91
alfredan <alfredan@kth.se>
2016-09-15 07:20:17 +0200

    King and Rook now keep track of whether they have previously moved (#2)

--- ChessLib/src/main/java/indaplusplus/alfredan/hw2and3/chesslib/pieces/King.java
+++ ChessLib/src/main/java/indaplusplus/alfredan/hw2and3/chesslib/pieces/King.java
@@ -1,5 +1,6 @@
 package indaplusplus.alfredan.hw2and3.chesslib.pieces;
 
+import indaplusplus.alfredan.hw2and3.chesslib.MutableBoard;
 import indaplusplus.alfredan.hw2and3.chesslib.util.MoveSet;
 
 /**
@@ -12,12 +13,36 @@ public final class King extends TemplatePiece {
     { 1,  1}, {-1,  1}, {-1, -1}, { 1, -1}  // diagonals
   });
   
+  /**
+   * Whether this king has ever moved. Used to determine whether or not you can castle.
+   */
+  public final boolean hasMoved;
+  
   public King(int team) {
+    this(team, false);
+  }
+  
+  public King(int team, boolean hasMoved) {
     super(team);
+    
+    this.hasMoved = hasMoved;
   }
   
   @Override
   protected MoveSet getSingleMoveSet() {
     return MOVESET;
+  }
+  
+  @Override
+  protected void makeMove(MutableBoard board, int xPos, int yPos, int moveX, int moveY) {
+    King newState;
+    
+    if (hasMoved) {
+      newState = this;
+    } else {
+      newState = new King(team, true);
+    }
+    
+    board.set(moveX, moveY, newState);
   }
 }
--- ChessLib/src/main/java/indaplusplus/alfredan/hw2and3/chesslib/pieces/Rook.java
+++ ChessLib/src/main/java/indaplusplus/alfredan/hw2and3/chesslib/pieces/Rook.java
@@ -1,5 +1,6 @@
 package indaplusplus.alfredan.hw2and3.chesslib.pieces;
 
+import indaplusplus.alfredan.hw2and3.chesslib.MutableBoard;
 import indaplusplus.alfredan.hw2and3.chesslib.util.MoveSet;
 
 /**
@@ -11,12 +12,36 @@ public final class Rook extends TemplatePiece {
     { 1,  0}, {-1,  0}, { 0,  1}, { 0, -1}
   });
   
+  /**
+   * Whether this rook has ever moved. Used to determine whether or not you can castle.
+   */
+  public final boolean hasMoved;
+  
   public Rook(int team) {
+    this(team, false);
+  }
+  
+  public Rook(int team, boolean hasMoved) {
     super(team);
+    
+    this.hasMoved = hasMoved;
   }
   
   @Override
   protected MoveSet getRepeatableMoveSet() {
     return MOVESET;
+  }
+  
+  @Override
+  protected void makeMove(MutableBoard board, int xPos, int yPos, int moveX, int moveY) {
+    Rook newState;
+    
+    if (hasMoved) {
+      newState = this;
+    } else {
+      newState = new Rook(team, true);
+    }
+    
+    board.set(moveX, moveY, newState);
   }
 }

07dd2df5d7709bdcb42dc68c575522447506064f
alfredan <alfredan@kth.se>
2016-09-15 03:01:40 +0200

    Removed old project

--- ChessLib/src/main/java/indaplusplus/alfredan/hw2and3/chesslib/BoardEventListener.java
+++
@@ -1,9 +0,0 @@
-package indaplusplus.alfredan.hw2and3.chesslib;
-
-public interface BoardEventListener {
-  
-  /**
-   * Called whenever an event occurs on the board.
-   */
-  void boardEvent(BoardEvent event);
-}

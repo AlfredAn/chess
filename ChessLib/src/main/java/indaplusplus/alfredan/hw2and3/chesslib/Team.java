af8aabccf5e6e1b9bc632bd093256f4e3f981084
alfredan <alfredan@kth.se>
2016-09-15 03:54:48 +0200

    Made Team uninstantiable

--- ChessLib/src/main/java/indaplusplus/alfredan/hw2and3/chesslib/Team.java
+++ ChessLib/src/main/java/indaplusplus/alfredan/hw2and3/chesslib/Team.java
@@ -4,7 +4,9 @@ package indaplusplus.alfredan.hw2and3.chesslib;
  * Contains constants for the black and white players in a standard chess game.
  * The team is stored as an int to allow for more than two players.
  */
-public class Team {
+public final class Team {
+  
+  private Team() {}
   
   public static final int BLACK = 0, WHITE = 1;
 }

57debfc7a9c2afe49fee859892e8bb7d9b440a14
alfredan <alfredan@kth.se>
2016-09-20 20:10:56 +0200

    Added ChessBoardDrawer

---
+++ ChessGame/core/src/indaplusplus/alfredan/hw2and3/chessgame/ChessBoardDrawer.java
@@ -0,0 +1,12 @@
+package indaplusplus.alfredan.hw2and3.chessgame;
+
+import indaplusplus.alfredan.hw2and3.chesslib.Board;
+
+final class ChessBoardDrawer {
+  
+  private ChessBoardDrawer() {}
+  
+  public static void drawChessBoard(Draw draw, Board board, int x, int y, int width, int height) {
+    
+  }
+}

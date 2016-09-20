c9f06cdd41003773a7292aedac59d53db4b30496
Fredrik Sebek <fsebek@kth.se>
2016-09-20 22:14:00 +0200

    Class for drawing the chess game.

--- ChessGame/core/src/indaplusplus/alfredan/hw2and3/chessgame/ChessBoardDrawer.java
+++ ChessGame/core/src/indaplusplus/alfredan/hw2and3/chessgame/ChessBoardDrawer.java
@@ -1,13 +1,66 @@
 package indaplusplus.alfredan.hw2and3.chessgame;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import indaplusplus.alfredan.hw2and3.chesslib.Board;
+import indaplusplus.alfredan.hw2and3.chesslib.Team;
+import indaplusplus.alfredan.hw2and3.chesslib.game.StandardChessGame;
+import indaplusplus.alfredan.hw2and3.chesslib.pieces.Rook;
 
 final class ChessBoardDrawer {
-  
-  private ChessBoardDrawer() {}
-  
-  public static void drawChessBoard(Draw draw, OrthographicCamera cam, Board board, int x, int y, int width, int height) {
+
+  private static boolean isWhite = true;
+
+  private ChessBoardDrawer() {
+  }
+
+
+  public static void drawChessBoard(Draw draw, OrthographicCamera cam, Board board, int x, int y, int width, int height, int mouseX, int mouseY) {
+
+
+
+   
+    draw.shapes.setProjectionMatrix(cam.combined);
+
+    draw.sprites.enableBlending();
+    draw.sprites.setProjectionMatrix(cam.combined);
+
+    for (int i = 0; i < 8; i++) {
+      for (int k = 0; k < 8; k++) {
+         draw.shapes.begin(ShapeRenderer.ShapeType.Filled);
+        if (isWhite) {
+          draw.shapes.setColor(Color.valueOf("FFCE9E"));
+          isWhite = false;
+        } else {
+          draw.shapes.setColor(Color.valueOf("D18B47"));
+          isWhite = true;
+        }
+        draw.shapes.rect(k * 68, i * 68, 64, 64);
+        draw.shapes.end();
+        draw.sprites.begin();
+        if (board.get(k,7 - i) != null) {
+        draw.sprites.draw(Sprites.getChessPiece(board.get(k, 7 - i)), k * 68, i * 68, 64, 64);
+        }
+        draw.sprites.end();
+      }
+      if (i % 2 == 1) {
+        isWhite = true;
+      } else {
+        isWhite = false;
+      }
+
+    }
     
+    
+
+
+
+
+    //draw.shapes.
+
+
+
+
   }
 }

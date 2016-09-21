7d3ca2c803bfeeaf4fe407942d49047495125da1
Fredrik Sebek <fsebek@kth.se>
2016-09-21 20:52:06 +0200

    Added blue background.

--- core/src/indaplusplus/alfredan/hw2and3/core/ChessBoardDrawer.java
+++ core/src/indaplusplus/alfredan/hw2and3/core/ChessBoardDrawer.java
@@ -5,24 +5,56 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import indaplusplus.alfredan.hw2and3.chesslib.Board;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.utils.Align;
+
+
 import indaplusplus.alfredan.hw2and3.chesslib.util.IntVector2;
+import static indaplusplus.alfredan.hw2and3.core.ChessGame.HEIGHT;
+import static indaplusplus.alfredan.hw2and3.core.ChessGame.WIDTH;
 import java.util.List;
 
 final class ChessBoardDrawer {
 
   private static boolean isWhite = true;
+  
+  private static String text = "Test";
 
   private ChessBoardDrawer() {}
 
   public static void drawChessBoard(Draw draw, OrthographicCamera cam, Board board, int x, int y, int width, int height, int mouseX, int mouseY, int hiddenX, int hiddenY) {
 
     draw.shapes.setProjectionMatrix(cam.combined);
-
     draw.enableBlending();
     draw.sprites.setProjectionMatrix(cam.combined);
+    
+       
+    BitmapFont bitmapFont = Fonts.arial32;
+    bitmapFont.setColor(Color.BLACK);
+
+    
+    String[] letters = { "A", "B", "C", "D", "E", "F", "G", "H"};
+    String[] numbers = { "1", "2", "3", "4", "5", "6", "7", "8"};
+
+    
+   
+    
+    draw.shapes.begin(ShapeRenderer.ShapeType.Filled);    
+    draw.shapes.setColor(Color.valueOf("#5EA1D4"));
+    draw.shapes.rect(0, 0, WIDTH, HEIGHT);
+    draw.shapes.end();
+    
 
     for (int i = 0; i < 8; i++) {
       for (int k = 0; k < 8; k++) {
+        
+          
+        draw.sprites.begin();
+        Fonts.arial32.setColor(Color.BLACK);
+        Fonts.arial32.draw(draw.sprites, text, WIDTH / 2, 100, 100, Align.center, false);
+    
+        draw.sprites.end();
+
         draw.shapes.begin(ShapeRenderer.ShapeType.Filled);
         if (isWhite) {
           draw.shapes.setColor(Color.valueOf("FFCE9E"));
